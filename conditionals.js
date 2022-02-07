8430e3e000c003eb95e8c075f176f0f4b50193e2
Tony Yu <tony.kwangjong.yu@gmail.com>
2022-02-07 23:04:40 +0000

    Add more conditionhal examples

--- conditionals.js
+++ conditionals.js
@@ -10,7 +10,7 @@ if(cold){
 
 console.log("Now you're ready to go outside!")*/
 
-const temperature = 0
+/*const temperature = 0
 
 if (temperature < 0){
   console.log("Make sure you pick out a scarf!")
@@ -20,4 +20,20 @@ if (temperature < 0){
   console.log("Short sleeves are fine.")
 }
 
-console.log("Now you're ready to go outside!")
+console.log("Now you're ready to go outside!")*/
+
+/*const isCitizen = true 
+const age = 26 
+
+if(isCitizen && age > 18){
+  console.log("You are eligible to vote.")
+}
+*/
+
+// if (temperature < -40 || temperature > 40){
+//   console.log("Maybe going outside isn't such a great idea...")
+// }
+
+if (!raining){
+  console.log("Leave your umbrella at home!")
+}
