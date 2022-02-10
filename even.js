750312fdcb4c9c059208cf481aac675bd60c887d
Tony Yu <tony.kwangjong.yu@gmail.com>
2022-02-10 01:05:08 +0000

    functions exercise

---
+++ even.js
@@ -0,0 +1,11 @@
+const isEven = function (num) {
+  return num % 2 === 0 
+}
+
+// const tenIsEven = isEven(10);
+// const elevenIsEven = isEven (11);
+
+console.log(isEven(10))
+console.log(isEven(11))
+
+// num % 2 === 0 means that if a number is divisible(?) by 2 then it equates to even, which is also true. 
---
+++ hello.js
@@ -0,0 +1,22 @@
+// const sayHello = function () {
+//   console.log("Hello, world");
+// }
+// sayHello()
+
+// const sayHello = function (name) { // (name) = parameter
+//   console.log("Hello, " + name)
+// }
+// sayHello("Caliban");
+// sayHello("Miranda");
+// sayHello("Ferdinand");
+
+// const sayHelloToConsole = function (name) { 
+//   console.log("Hello, " + name)
+// }
+// sayHelloToConsole('John')
+
+const returnSayHello = function (name) { 
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting)
